8f2785dc4f40910ce908be8dfe8dfca0d65703f8
Nathoriz <nathortiz_41@hotmail.com>
2017-12-30 01:15:42 -0500

    Añadiendo evento mouseover y clases

--- js/filter.js
+++ js/filter.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
+  // Elementos
+  var $food = $(".img-food");
+  var $filterRestaurants = $("#filterRestaurants");  // Input
+  // Opciones
+  var $mar = $("#mar");
+  var $vegetariana = $("#vegetariana");
+  var $china = $("#china");
+  var $mexicana = $("#mexicana");
+  var $criolla = $("#criolla");
+  
+  
+  // Eventos
+
+  // Aparición del nombre del restaurante cuando se haga el evento mouseover
+  $food.mouseover(function() {
+    var restaurantName = $(this).data("restaurant");
+    var addInfo = $("<div class='info container-flex-column'><p>" + restaurantName + "<p><div>");
+    $(this).append(addInfo);
+  });
     
   });
